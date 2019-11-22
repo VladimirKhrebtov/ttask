8faad76f0d85ad5536d4be9680c30320d6db71c1
VladimirKhrebtov <vladimir.h@belvg.com>
2019-11-22 21:20:04 +0300

    test

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get('/page-not-found', (req, res, next) => {
 app.use('/', (req, res, next) => {
     res.redirect('/page-not-found');
 });
-app.listen(PORT, '0.0.0.0', () => { console.log(`Port is running on port ${PORT}`)});
+app.listen(PORT, '0.0.0.0', () => { console.log(`Server is running on port ${PORT}`)});
 dbConnection.dbConnection();
