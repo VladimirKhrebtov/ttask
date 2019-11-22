91f89579b557b82cfee8426d2e59a2c515c07453
VladimirKhrebtov <vladimir.h@belvg.com>
2019-11-22 18:43:27 +0300

    port testing

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const dbConnection = require('./utils/database');
 const app = express();
-const PORT = process.env.port || 4444;
+const PORT = process.env.port || 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
